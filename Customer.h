c0a9be7b59bd63e276d34c37fbe9b8e7bf166c71
bclinkscale <34066182+bclinkscale@users.noreply.github.com>
2018-12-01 18:09:03 -0500

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -8,13 +8,19 @@ using namespace std;
 
 class Customer{
 private:
-  //Creating a stack to hold the items inside the cart
-  stack <string> cart;
+  //Creating a queue to hold the items inside the cart
+  int *queueArray;
+  int queueLen;
+  int front;
+  int rear;
+  int numItems;
 
 public:
   //Functions that will allow the user to manipulate the items inside the cart
   void add_to_cart(string);
-  void remove_from_cart(string);
+  void remove_from_cart(string &);
+  bool isEmpty() const; 
+  bool isFull() const;
   string view_cart();
 
 
