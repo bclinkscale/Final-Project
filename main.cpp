cce8a1d093a4d68215036cadc19470500228ef88
bclinkscale <34066182+bclinkscale@users.noreply.github.com>
2018-12-01 18:49:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,17 @@ using namespace std;
 
 int main(){
   fstream dataFile;
+  string line;
+  dataFile.open(Inventory.txt. ios::in);
+  if (dataFile.is_open()){
+    while (getline(dataFile, line)){
+      cout << line << endl;
+    }
+    dataFile.close();
+  }
+  else{
+    cout << "Can't read File!" << endl;
+    
 
-
+return 0;
 }
