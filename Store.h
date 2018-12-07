3f4f298e1df48e3d7360d5c46b242c273abc0445
desvignesd <43765367+desvignesd@users.noreply.github.com>
2018-12-06 21:31:23 -0500

    Update Store.h

--- Store.h
+++ Store.h
@@ -32,38 +32,4 @@ int getPrice() const
 
 };
 
-#endif#ifndef STORE_H
-#define STORE_H
-
-#include <string>
-#include <stack>
-
-using namespace std;
-
-class Store{
-private:
-  string itemName;
-  double price;
-
-
-public:
-  Store (string i, double p) {
-    itemName = i ;
-    price = p;
-  };
-
-void setItemName(string i)
-{itemName = i;}
-
-void setPrice(double p)
-{price = p;}
-
-string getItemName() const
-{return itemName;}
-
-int getPrice() const
-{return price;}
-
-};
-
 #endif
